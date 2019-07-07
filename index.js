60eecd4889c40f41b7a8cdbf1d0929f2bf469517
Steven Freed <saf236@scarletmail.rutgers.edu>
2019-07-07 15:20:09 -0400

    KNN working, Naive Bayes train function needs prob tables with range for numbers and exact for strings

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ function readToMatrixAndVector(filePath, options={ skipHeader: true, delimiter: 
             if (!isNaN(features[feat])) // if number
                 features[feat] = Number(features[feat]);
             else if (String(features[feat].toLowerCase()) == 'true') // true
-                features[feat] = true;
+                features[feat] = 1;
             else if (String(features[feat].toLowerCase()) == 'false') // false
-                features[feat] = false;
+                features[feat] = 0;
             else // if string
                 features[feat] = features[feat].trim();
         }
@@ -116,37 +116,43 @@ function trainnb(trainingSet, classifiedSet) {
     }
     classTable['total'] = classifiedSet.length;
 
-    let featTables = {};
+    console.log(classTable);
+    let featTable = {};
     // makes feature tables
     for (let v = 0; v < trainingSet.length; v++) {
-        for (let f = 0; f < trainingSet[v].length; f++) {
-            if (featTables[f] === undefined)
-                featTables[f] = {};
-            else if (featTables[f][trainingSet[v][f]] === undefined) {
-                featTables[f][trainingSet[v][f]] = {};
-                featTables[f][trainingSet[v][f]][classifiedSet[v]] = 1;
-            } else if (featTables[f][trainingSet[v][f][classifiedSet[v]]] === undefined)
-                featTables[f][trainingSet[v][f]][classifiedSet[v]] = 1;
-            else
-                featTables[f][trainingSet[v][f]][classifiedSet[v]] += 1;
+        for (let feat of trainingSet[v]) {
+            if (featTable[feat] === undefined) {
+                featTable[feat] = {};
+                featTable[feat][classifiedSet[v]] = 1;
+            } else {
+                
+                featTable[feat][classifiedSet[v]] += 1;
+            }
         }
     }
-    nbTrainedSet = [featTables, classTable]; // sets a global trained set variable
+    console.log(featTable);
+
+    //nbTrainedSet = [featTables, classTable]; // sets a global trained set variable
 }
  
+/**
+ * Trains the KNN algorithm by setting a global variable of the trained data
+ * 
+ * @param {[[]]} trainingSet    feature matrix of training data
+ * @param {[]} classificationVector class labels vector
+ */
 function trainknn(trainingSet, classificationVector) {
 
     for(let i = 0; i < classificationVector.length; i++) {
-        let FLAG = 0;
-        for(let key of Object.keys(knnTrainedSet)) {
-           if (classificationVector[i] === key) {
-               FLAG = 1;
-               break;
-           }
-        }
 
-        if (!FLAG)
+        for (let f = 0; f < trainingSet[i].length; f++) {
+            if (typeof(trainingSet[i][f]) == 'string')
+                trainingSet[i][f] = trainingSet[i][f].hashCode();
+        }
+      
+        if (!Object.keys(knnTrainedSet).includes(classificationVector[i][0])) {
             knnTrainedSet[classificationVector[i]] = [];
+        }
         knnTrainedSet[classificationVector[i]].push(trainingSet[i]);
     } 
 }
@@ -157,9 +163,13 @@ function trainp(trainingSet, classifiedSet) {
 
 /**
  * 
- * @param {[[]]} data  feature matrix 
+ * @param {[[]]} matrix  feature matrix 
  */
-function naiveBayes(data, options={}) {
+function naiveBayes(matrix, options={}) {
+
+    let data = parseInput(matrix); // parses all input types to numbers and returns feature matrix
+    if (!data)
+        throw new Error("Parameter 'data' for function 'KNN' could not be parsed to valid data types.");
 
     if(nbTrainedSet === undefined)
         throw new Error("Running the 'train' function is required to run before the 'KNN' function.");
@@ -175,14 +185,19 @@ function naiveBayes(data, options={}) {
         for (let _class of classes) { // loops through classes to get final classification
             let prob = 1;
             for (let f = 0; f < data[v].length; f++) { // loops through features of vector
+
                 let yns = featTables[f][data[v][f]]; // class counts for the particular feature f
               
-                if (yns === undefined)
+                if (yns === undefined) {
                     prob *= 0.5;
-                else if (yns[_class] === undefined)
+                  
+                } else if (yns[_class] === undefined) {
                     prob *= 1 / classTable[_class];
-                else
+                    
+                } else {
                     prob *= yns[_class] / classTable[_class];
+                   
+                }
             }
          
             if (probs[v] === undefined)
@@ -202,11 +217,41 @@ function naiveBayes(data, options={}) {
 }
 
 /**
+ * Parses the feature matrix for KNN algorithm because it requires data to be 
+ * numeric, this function parses strings and booleans to numbers
+ * 
+ * @param {[[]]} data your feature matrix 
+ */
+function parseInput(data) {
+
+    let parsedMatrix = [];
+    for (let vector of data) {
+        for (let feat = 0; feat < vector.length; feat++) {
+            if (!isNaN(vector[feat])) // if number
+                vector[feat] = Number(vector[feat]);
+            else if (vector[feat] === true) // true
+                vector[feat] = 1;
+            else if (vector[feat] == false) // false
+                vector[feat] = 0;
+            else // if string
+                vector[feat] = vector[feat].hashCode();
+        }
+        parsedMatrix.push(vector);
+    }   
+   
+    return parsedMatrix;
+}
+
+/**
  * 
  * @param {[[]]} data data you wish to be classified
  * @param {{ k: 3 }} options An optional param. k to specify how many neighbors to use
  */
-function KNN(data, options={'k': 3 }) {
+function KNN(matrix, options={'k': 3 }) {
+    let data = parseInput(matrix); // parses all input types to numbers and returns feature matrix
+    if (!data)
+        throw new Error("Parameter 'data' for function 'KNN' could not be parsed to valid data types.");
+    
     let classifications = []; // final results
     let kSet = []; // set of k nearest points
     
@@ -214,18 +259,19 @@ function KNN(data, options={'k': 3 }) {
         throw new Error("Running the 'train' function is required to run before the 'KNN' function.");
 
     for(let d = 0; d < data.length; d++) { // loops all items you want to classify
-
-        for(let key = 0; key < knnTrainedSet.length; key++) { // loops vectors in training set
-            for(let feature of knnTrainedSet[key]) { // loops features in training vector
-                // calculates euclidean distance between training vectors
-                // and vectors to be classified
-                let ed = (function euclideanDistance(feat, pt) { 
-                    let difs = [feat].map((f, index) => Math.abs(f - pt[index])); 
-                    let sqSum = difs.map((d) => Math.pow(d, 2));
-                    return Math.sqrt(sqSum);
-                })(feature, data[d]);
-
-                kSet.push([key, feature, ed]);
+        for(let key of Object.keys(knnTrainedSet)) { // loops vectors in training set
+            for (let vector of knnTrainedSet[key]) {
+                for(let feature of vector) { // loops features in training vector
+                    // calculates euclidean distance between training vectors
+                    // and vectors to be classified
+                    let ed = (function euclideanDistance(feat, pt) { 
+                        let difs = [feat].map((f, index) => Math.abs(f - pt[index])); 
+                        let sqSum = difs.map((d) => Math.pow(d, 2));
+                        return Math.sqrt(sqSum);
+                    })(feature, data[d]);
+            
+                    kSet.push([key, feature, ed]);
+                }
             }
         }
 
@@ -233,32 +279,29 @@ function KNN(data, options={'k': 3 }) {
         kSet.sort((a, b) => a[2] - b[2]);
         kSet = kSet.slice(0, options.k);
     
-        // creates the frequency dictionary containing { class : frequency }
+        // creates the frequency empty dictionary containing { class : frequency }
         let freqSet = {};
-        for(let i of kSet) {
+        for(let i of kSet)
             freqSet[i[0]] = 0;
-        }
 
         // if only one key exists it returns that class
         if(Object.keys(freqSet) === 1 || Object.keys(freqSet) === options.k)
             return Object.keys(freqSet)[0];
         else {
-            for(let i of kSet) {
+            for(let i of kSet)
                 freqSet[i[0]] += 1;
-            }
         }
 
         // iterates through the frequency dictionary to get the most frequent closest k class
-        let topClass = undefined;
-        for(let k0 of Object.keys(freqSet)) {
-            for(let k1 of Object.keys(freqSet)) {
-                if(k0 >= k1)
-                    topClass = k0;
-            }
-        }
-
+        let keys = Object.keys(freqSet);
+        let topClass = keys[0];
+        for(let k0 = 0; k0 < keys.length; k0++)
+            for(let k1 = k0 + 1; k1 < keys.length; k1++)
+                if(freqSet[keys[k0]] >= freqSet[keys[k1]])
+                    topClass = keys[k0];
+    
         classifications.push(topClass);
-    }
+    } 
 
     return classifications;
 }
@@ -266,3 +309,19 @@ function KNN(data, options={'k': 3 }) {
 function perceptron(data, options) {
     return options;
 }
+
+/**
+ * Converts a string to a 32 bit integer hashcode by trimming the string
+ * and converting it to lowercase before generating the hash
+ */
+String.prototype.hashCode = function() { 
+    let hash = 0;
+    if (this.trim().length === 0) return hash;
+
+    for (let i = 0; i < this.trim().length; i++) {
+        let char = this.trim().toLowerCase().charCodeAt(i);
+        hash  = ((hash << 5) - hash) + char;
+        hash |= 0; 
+    }
+    return hash;
+  }
